b848a5935fb0f9e11ce7c6c0f20520abb8c27e75
saraalmulla03 <sma8998@nyu.edu>
2023-12-09 17:07:53 +0400

    SARA'S PORTFOLIO

---
+++ sara's portfolio/public_html/script.js
@@ -0,0 +1,11 @@
+// cursor dot
+const cursorDot = document.querySelector("[data-cursor-dot]");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+});
